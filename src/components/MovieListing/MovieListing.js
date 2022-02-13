2c76af49490529f0d890ce8ee3d4e6e85e3d986e
ualiid <moraoualid@gmail.com>
2022-02-13 13:26:58 +0100

    movies app'

---
+++ src/components/MovieListing/MovieListing.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { getAllMovies, getAllShows } from "../../features/movies/movieSlice";
+import MovieCard from "../MovieCard/MovieCard";
+import "./MovieListing.scss";
+const MovieListing = () => {
+  const movies = useSelector(getAllMovies);
+  const shows = useSelector(getAllShows);
+  
+
+  const renderMovies =
+    movies.Response === "True" ? (
+      movies.Search.map((movie, index) => {
+       
+        return(
+        <MovieCard key={index} {...movie} />)
+        })
+    ) : (
+      <div className="movies-error">
+        <h3>{movies.Error}</h3>
+      </div>
+    );
+
+  const renderShows =
+    shows.Response === "True" ? (
+      shows.Search.map((show, index) =>{ 
+      
+      return (
+      <MovieCard key={index} {...show} />
+      )
+    }
+      )
+    ) : (
+      <div className="shows-error">
+        <h3>{shows.Error}</h3>
+      </div>
+    );
+  return (
+    <div >
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movie-container">{renderMovies}</div>
+      </div>
+      <div className="show-list">
+        <h2>Cartoon</h2>
+        <div className="movie-container">{renderShows}</div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieListing;
